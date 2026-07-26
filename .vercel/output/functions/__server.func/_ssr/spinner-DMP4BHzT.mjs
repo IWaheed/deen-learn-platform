1efe5ecc587a504d417e9ac618526244e923cdeb
IWaheed <iwaheed@atheer-ct.com>
2026-07-26 14:07:04 +0200

    Remove .vercel build artifacts from git, add to .gitignore

--- .vercel/output/functions/__server.func/_ssr/spinner-DMP4BHzT.mjs
+++
@@ -1,9 +0,0 @@
-import { P as require_jsx_runtime } from "../_libs/@radix-ui/react-alert-dialog+[...].mjs";
-import { m as LoaderCircle } from "../_libs/lucide-react.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/spinner-DMP4BHzT.js
-var import_jsx_runtime = require_jsx_runtime();
-function Spinner({ className = "h-4 w-4" }) {
-	return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(LoaderCircle, { className: `animate-spin ${className}` });
-}
-//#endregion
-export { Spinner as t };
